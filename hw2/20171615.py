3f5660e01b1c39ef333bf4158d188d3d1fdc14c6
Noh Seonghwan <nsh1998@kookmin.ac.kr>
2021-04-28 13:29:08 +0900

    web server

--- hw2/20171615.py
+++ hw2/20171615.py
@@ -1,33 +1,32 @@
-# Computer Network, 2021
-#
-# 20171615 - HTTP file server implementation
-#
-# Author: Seonghwan Noh
-# Date: April 26th, 2021
+# Student ID: 20171615
+# Name: Seonghwan Noh
+
 import socket
 import sys
 import os
 
 
-BUFSIZE = 1 << 10
+BUFSIZE = 1024
 
 
-def handle_file(msg: str, client_socket) -> (int, int):
+# get filename, filesize
+
+
+def send_responseMsg(msg, client_socket):
     filename = msg.split('\r\n')[0].split('/')[1].split()[0]
     sent, filesize = 0, 0
 
-    print(filename)
+    # print(filename)
 
     try:
         filesize = os.path.getsize(filename)
     except:
-        header = f'HTTP/1.0 404 NOT FOUND\r\nConnection: close\r\nContent-Length: 0\r\nContent-Type: text/html\r\n\r\n'.encode(
-        )
-        client_socket.send(header)
+        responseMsg = 'HTTP/1.0 404 NOT FOUND\r\nConnection: close\r\nContent-Length: 0\r\nContent-Type: text/html\r\n\r\n'.encode()
+        client_socket.send(responseMsg)
     else:
-        header = f'HTTP/1.0 200 OK\r\nConnection: close\r\nContent-Length: {filesize}\r\nContent-Type: text/html\r\n\r\n'.encode(
-        )
-        client_socket.send(header)
+        responseMsg = (
+            'HTTP/1.0 200 OK\r\nConnection: close\r\nContent-Length: {}\r\nContent-Type: text/html\r\n\r\n'.format(filesize)).encode()
+        client_socket.send(responseMsg)
 
         with open(filename, 'rb') as f:
             data = f.read(BUFSIZE)
@@ -35,36 +34,47 @@ def handle_file(msg: str, client_socket) -> (int, int):
                 sent += client_socket.send(data)
                 data = f.read(BUFSIZE)
 
-    return sent, filesize
+    return sent, filesize, filename
 
 
 if len(sys.argv) != 2:
     print("usage: %s portnum\n" % sys.argv[0])
     exit()
 
-portNum = int(sys.argv[1])
-ip = '127.0.0.1'
-
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serverSocket.bind((ip, portNum))
-serverSocket.listen()
-
 print("Student ID: 20171615")
 print("Name: Seonghwan Noh")
 
+portNum = int(sys.argv[1])
+ip = '127.0.0.1'
+
+# set server_socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+try:
+    serverSocket.bind((ip, portNum))
+    serverSocket.listen()
+except OSError:
+    print('Address already in use!')
+    exit()
+
+
 while True:
+    # set client_socket
     clientSocket, clientAddr = serverSocket.accept()
     requestMsg = clientSocket.recv(BUFSIZE).decode()
     header = requestMsg.split('\r\n\r\n')[0]
-    header_field_len = len(header.split('\r\n'))-1
+    header_field_num = len(header.split('\r\n'))-1
 
     print('Connection : Host IP {}, Port {}, socket {}'.format(
         clientAddr[0], clientAddr[1], clientSocket.fileno()))
-    print(f'{requestMsg}{header_field_len} headers')
+    print(requestMsg)
+    print('{} headers'.format(header_field_num))
 
-    sent, filesize = handle_file(requestMsg, clientSocket)
-    print(f'finish {sent} {filesize}')
+    sent, filesize, filename = send_responseMsg(requestMsg, clientSocket)
+    if sent > 0 and filesize > 0:
+        print('finish {} {}\n'.format(sent, filesize))
+    else:
+        print('Server Error : No such file ./{}!\n'.format(filename))
     clientSocket.close()
 
 serverSocket.close()
