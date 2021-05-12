3046f4a400952481f82b89e7d96a7166afd9bc35
Noh Seonghwan <nsh1998@kookmin.ac.kr>
2021-05-13 01:40:19 +0900

    hw3

---
+++ hw3/20171615.py
@@ -0,0 +1,53 @@
+# Student ID: 20171615
+# Name: Seonghwan Noh
+
+import socket
+import select
+import sys
+
+BUFSIZE = 1024
+
+
+def display():
+    print('Student ID : 20171615')
+    print('Name : Seonghwan Noh')
+
+
+if len(sys.argv) != 2:
+    print('Usage: {} <tcpport> \n'.format(sys.argv[0]))
+    exit(1)
+
+display()
+
+portNum = int(sys.argv[1])
+ip = '127.0.0.1'
+
+# set serverSocket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+try:
+    serverSocket.bind((ip, portNum))
+    serverSocket.listen(8)
+    inputList = [serverSocket]
+
+except OSError:
+    print('Address already in use!')
+    exit()
+
+while True:
+    rlist, wlist, xlist = select.select(inputList, [], [])
+    for sock in rlist:
+        if sock == serverSocket:
+            clientSocket, clientAddr = serverSocket.accept()
+            inputList.append(clientSocket)
+            print('Connection from host {}, port {}, socket{}\n'.format(
+                clientAddr[0], clientAddr[1], clientSocket.fileno()))
+        else:
+            msg = sock.recv(BUFSIZE).decode()
+            if msg:
+                sock.send(msg)
+            else:
+                sock.close()
+                inputList.remove(sock)
+
+serverSocket.close()
