23c0d292c85b0c588008b10af771eaf076f62c3e
Noh Seonghwan <nsh1998@kookmin.ac.kr>
2021-06-04 13:22:25 +0900

    hw4 end

--- hw4/20171615.py
+++ hw4/20171615.py
@@ -13,6 +13,21 @@ def display():
     print('Name : Seonghwan Noh')
 
 
+def order(msg):
+    msg_list = msg.split()
+    if msg_list[0] == '@talk':
+        host_name = msg_list[1]
+        tc_port = int(msg_list[2])
+        clientSocket.connect((host_name, tc_port))
+        inputList.append(clientSocket)
+    elif msg_list[0] == '@quit':
+        exit()
+    else:
+        msg = user_id + ' : ' + msg
+        clientSocket.send(msg.encode())
+    print(user_id + '>')
+
+
 if len(sys.argv) != 3:
     print('Usage: {} <tcpport> \n'.format(sys.argv[0]))
     exit(1)
@@ -23,7 +38,7 @@ ip = '127.0.0.1'
 port_num = int(sys.argv[1])
 user_id = sys.argv[2]
 
-# set serverSocket
+# set serverSocket, clientSocket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -38,8 +53,8 @@ except OSError:
     exit()
 
 print(user_id + ">")
-flag = True
-while flag:
+
+while True:
     rlist, wlist, xlist = select.select(inputList, [], [])
     for sock in rlist:
         # Input from the client for new connection
@@ -48,24 +63,16 @@ while flag:
             inputList.append(clientSocket)
             print(
                 f'connection from host {clientAddr[0]}, port {clientAddr[1]}, socket {clientSocket.fileno()}')
+        # order(stdin)
         elif sock == sys.stdin:
             msg = sys.stdin.readline()
-            msg_list = msg.split()
-            if msg_list[0] == '@talk':
-                host_name = msg_list[1]
-                tc_port = int(msg_list[2])
-                clientSocket.connect((host_name, tc_port))
-                inputList.append(clientSocket)
-            elif msg_list[0] == '@quit':
-                flag = False
-            else:
-                msg = user_id + ' : ' + msg
-                clientSocket.send(msg.encode())
-            print(user_id + '>')
+            order(msg)
+        # input data in client
         else:
             data = sock.recv(BUFSIZE).decode()
             if data:
                 print(data)
+            # client close socket
             else:
                 print(f'Connection Closed {sock.fileno()}')
                 inputList.remove(sock)
